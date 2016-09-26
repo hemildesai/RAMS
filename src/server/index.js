5efd33009df10f6898c3a95807a8969a14c343e2
hemildesai <desai38@purdue.edu>
2016-09-26 00:29:25 -0400

    Create index.js as entrypoint for the API

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import morgan from 'morgan';
+import passport from 'passport';
+
+import db from '../db';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV == "development") {
+  app.use(morgan('dev'));
+}
+
+app.use(compression());
+app.use(passport.initialize());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("app listening on port " + port);
+});
+
+export default app;
