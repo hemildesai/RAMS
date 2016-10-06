44c233dad959bb43a2c24281874af8fcd36b1934
hemildesai <desai38@purdue.edu>
2016-10-06 17:28:31 -0400

    Logged in user can now change password

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -14,3 +14,25 @@ export function postUser(req, res) {
     res.json({success: false, errors: err});
   });
 }
+
+export function changeUserPassword(req, res) {
+  User.verifyUser(req.user.attributes.username, req.body.old_password)
+    .then(user => {
+      user
+        .save({
+          password: req.body.new_password
+        })
+        .then(user => {
+          res.json({success: true, message: "Password changed"});
+        })
+        .catch(err => {
+          res.json({success: false, message: "Password change failed"});
+        });
+    })
+    .catch(User.NotFoundError, () => {
+      res.json({success: false, error: "User not found"});
+    })
+    .catch(err => {
+      res.json({success: false, error: "Error in changing password"});
+    });
+}
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import {postUser} from '../controllers/user.js';
+import passport from 'passport';
+import {changeUserPassword, postUser} from '../controllers/user.js';
 
 const router = express.Router();
 
 router.route('/')
   .post(postUser);
+
+router.route("/change")
+  .post(passport.authenticate("jwt", {session: false}), changeUserPassword);
 
 module.exports = router;
