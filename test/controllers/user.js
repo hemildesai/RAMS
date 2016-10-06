2868d2df1669eb1398c6b5705b962ee7fbbeb8a5
hemildesai <desai38@purdue.edu>
2016-10-06 17:28:31 -0400

    Logged in user can now change password

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -1,3 +1,4 @@
+import {authenticate_user} from '../helpers/authentication_helper.js';
 
 describe("User controller tests", () => {
 
@@ -22,6 +23,65 @@ describe("User controller tests", () => {
           expect(res.body.success).to.eq(true);
           expect(res.body.user).to.not.be.undefined;
           expect(res.body.user.username).to.eq("test");
+          done();
+        });
+    });
+
+    it("should change a users password if old password is correct", (done) => {
+      authenticate_user()
+        .then((token) => {
+          chai.request(server)
+            .post("/api/users/change")
+            .set("x-access-token", token)
+            .send({
+              old_password: "12345",
+              new_password: "123456"
+            })
+            .end((err, res) =>  {
+              res.should.have.status(200);
+              expect(res.body.success).to.eq(true);
+              chai.request(server)
+                .post("/api/authenticate")
+                .send({
+                  username: "tu1",
+                  password: "123456"
+                })
+                .end((err, res) => {
+                  expect(res.status).to.eq(200);
+                  expect(res.body.success).to.eq(true);
+                  done();
+                });
+            });
+        });
+    });
+
+    it("should not change a users password if old password is incorrect", (done) => {
+      authenticate_user()
+        .then((token) => {
+          chai.request(server)
+            .post("/api/users/change")
+            .set("x-access-token", token)
+            .send({
+              old_password: "1234565",
+              new_password: "123456"
+            })
+            .end((err, res) =>  {
+              res.should.have.status(200);
+              expect(res.body.success).to.eq(false);
+              done();
+            });
+        });
+    });
+
+    it("should not change a users password if user is not signed in", (done) => {
+      chai.request(server)
+        .post("/api/users/change")
+        .send({
+          old_password: "1234565",
+          new_password: "123456"
+        })
+        .end((err, res) =>  {
+          res.should.have.status(401);
           done();
         });
     });
