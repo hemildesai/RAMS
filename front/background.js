c22e1cbc7b70d6adccd7a871d847a1abbbfa16ba
asamanta94 <asamanta@purdue.edu>
2016-10-07 03:49:27 -0400

    Login Page created

--- front/background.js
+++ front/background.js
@@ -1,4 +1,18 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	var tab_url = "https://www.google.com";
-	chrome.tabs.create({ url: tab_url });
+	var tab_url = "background.html";
+	var viewTabUrl = chrome.tabs.getURL(tab_url);
+
+	var views = chrome.extension.getViews();
+	for (var i = 0; i < views.length; i++) {
+		var view = views[i];
+
+		// If this view has the right URL and hasn't been used yet...
+		if (view.location.href == viewTabUrl) {
+
+		  // ...call one of its functions and set a property.
+		  view.setImageUrl(imageUrl);
+		  view.imageAlreadySet = true;
+		  break; // we're done
+		}
+  }
 });
