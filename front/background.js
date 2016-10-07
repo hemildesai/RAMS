ba7ed0fdd5f06b7da75810df70b7de7000445548
asamanta94 <asamanta@purdue.edu>
2016-10-07 06:58:44 -0400

    URL copy on extension button click done

--- front/background.js
+++ front/background.js
@@ -1,18 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	var tab_url = "background.html";
-	var viewTabUrl = chrome.tabs.getURL(tab_url);
+	// Get current Tab URL
+	var curr_tab_url = tab.url;
 
-	var views = chrome.extension.getViews();
-	for (var i = 0; i < views.length; i++) {
-		var view = views[i];
-
-		// If this view has the right URL and hasn't been used yet...
-		if (view.location.href == viewTabUrl) {
-
-		  // ...call one of its functions and set a property.
-		  view.setImageUrl(imageUrl);
-		  view.imageAlreadySet = true;
-		  break; // we're done
-		}
-  }
+	// Open RAMS window to store the URL
+	var rams_user_tab = window.open("./front/usr_rsrc_tab.html");
+	if(rams_user_tab != null)
+		rams_user_tab.onload = function() {
+			rams_user_tab.document.getElementById("rsrc_input").value = curr_tab_url;
+		};
+	else
+		alert("Sorry\n");
 });
