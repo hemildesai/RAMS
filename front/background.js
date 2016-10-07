0ce941b473bc223f18bc0531d029ae7d805b9ba1
asamanta94 <asamanta@purdue.edu>
2016-10-07 09:40:25 -0400

    Added Image for home page. Created Dashboard and Home Page as well.

--- front/background.js
+++ front/background.js
@@ -6,12 +6,21 @@ chrome.browserAction.onClicked.addListener(function(tab){
 	// Get current Tab URL
 	var curr_tab_url = tab.url;
 
-	// Open RAMS window to store the URL
-	var rams_user_tab = window.open("./front/usr_rsrc_tab.html");
-	if(rams_user_tab != null)
-		rams_user_tab.onload = function() {
-			rams_user_tab.document.getElementById("rsrc_input").value = curr_tab_url;
-		};
+	// Check for empty URL
+	if(curr_tab_url == "about:blank" || curr_tab_url == "")
+	{
+		alert("Empty URL");
+		return;
+	}
 	else
-		alert("Sorry\n");
+	{
+		// Open RAMS window to store the URL
+		var rams_user_tab = window.open("./front/usr_rsrc_tab.html");
+		if(rams_user_tab != null)
+			rams_user_tab.onload = function() {
+				rams_user_tab.document.getElementById("rsrc_input").value = curr_tab_url;
+			};
+		else
+			alert("Sorry\n");
+	}
 });
