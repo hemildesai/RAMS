1d8b9beab457af527cf7b4b998f27d32427507a5
System Administrator <root@Adits-MacBook-Pro.local>
2016-11-03 07:04:49 -0400

    Made changes to files to make jquery bootstrap work

---
+++ front/home_rams.js
@@ -0,0 +1,6 @@
+document.getElementById("home_login").addEventListener("click", login_function);
+
+function login_function()
+{
+	window.open("./login_sign_up.html", "_self");
+}
---
+++ front/usr_rsrc_tab.js
@@ -0,0 +1,6 @@
+document.getElementById("save_button").addEventListener("click", save_function);
+
+function save_function()
+{
+    window.open("./dashboard.html", "_self");
+}
